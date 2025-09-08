b8f464ec314003c2aa4734877754b03cdbf70398
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-08 12:36:46 +0600

    .

--- src/TestFX/TestFX.java
+++ src/TestFX/TestFX.java
@@ -6,7 +6,8 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-//EDIT VM
+//for javaFX setup
+//edit vm
 //--module-path "D:\UIU CSE\Codes & Libs\JavaLib\javafx-sdk-24.0.2\lib" --add-modules javafx.controls,javafx.fxml
 public class TestFX extends Application {
 
