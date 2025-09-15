cfefc7dbde3479411135735a6387771e3f186ec8
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-15 21:09:36 +0600

    update

---
+++ src/GameClient/Main.java
@@ -0,0 +1,26 @@
+package GameClient;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root= FXMLLoader.load(getClass().getResource("intro.fxml"));
+        stage.setTitle("Drawbbl");
+
+        Image appIcon = new Image(getClass().getResourceAsStream("icon.png"));
+        stage.getIcons().add(appIcon);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
